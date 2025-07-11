f8435e3a68548b620d7bdac0353d26421a376dfe
jaspreet kaur <jaspreet.kaur@fortis.com>
2025-07-12 02:26:08 +0530

    oneco postman script changes

--- OneCo/oneCo-generate-postman-collection.js
+++ OneCo/oneCo-generate-postman-collection.js
@@ -61,13 +61,16 @@ async function generatePostmanCollectionsByTransactionType() {
       request: {
         method: 'POST',
         header: [
-          { key: 'user-id', value: '{{ecomm_user_id}}' },
-          { key: 'user-key', value: '{{ecomm_user_key}}' },
+          { key: 'user-id', value: '{{user-id}}' },
+          { key: 'user-api-key', value: '{{user-api-key}}' },
           { key: 'Content-Type', value: 'application/json' },
+          { key: 'developer-id', value: '{{developer-id}}' },
+          { key: 'Accept', value: 'application/json' },
+          { key: 'access-token', value: '{{access-token}}' },
         ],
         body: { mode: 'raw', raw: jsonBody },
         url: {
-          raw: 'https://{{url}}/{{namespace}}/transactions',
+          raw: 'https://{{url}}/{{namespace}}/transactions/cc/sale/keyed',
           protocol: 'https',
           host: ['{{url}}'],
           path: ['{{namespace}}', 'transactions'],
